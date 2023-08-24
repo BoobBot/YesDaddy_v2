f935524fac72d493a4867a39ba13440157ddb4d7
Tom <hydrabott@gmail.com>
2023-08-24 13:49:02 -0400

    guess it's my problem now

--- utils/checks.py
+++ utils/checks.py
@@ -4,10 +4,6 @@ import discord
 from discord.ext import commands
 
 
-def datetime_to_discord_timestamp(dt):
-    return dt.strftime("<t:%Y-%m-%d %H:%M:%S>")
-
-
 def persistent_cooldown(rate, per, type=commands.BucketType.user):
     async def predicate(ctx, *args, **kwargs):
         now = datetime.datetime.utcnow()
@@ -20,13 +16,13 @@ def persistent_cooldown(rate, per, type=commands.BucketType.user):
             pass
 
         command_cooldown = document['cooldowns'].get(ctx.command.name)
-        print()
         if command_cooldown and (last_used := command_cooldown + datetime.timedelta(seconds=per)) > now:
             delta = last_used - now
             target_time = now + delta
+            epoch = round(target_time.timestamp())
             embed = discord.Embed(
-                title=f'You are on cooldown. Try again in {datetime_to_discord_timestamp(target_time)}.',
-                description=datetime_to_discord_timestamp(target_time), color=0x00ff00)
+                title=f'You are on cooldown. Try again in <t:{epoch}:R>.',
+                color=0x00ff00)
             await ctx.send(embed=embed)
             return False
 
