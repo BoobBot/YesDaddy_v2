75d36efccffd0d5683d5c8ef69a1e370328776f8
Tom <hydrabott@gmail.com>
2023-08-25 08:37:21 -0400

    revert as WTF reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- utils/checks.py
+++ utils/checks.py
@@ -1,33 +1,24 @@
 import datetime
 
-import discord
-import pytz
 from discord.ext import commands
 
 
 def persistent_cooldown(rate, per, type=commands.BucketType.user):
     async def predicate(ctx, *args, **kwargs):
         now = datetime.datetime.utcnow()
-        print(now)
         user_id = ctx.author.id
+
         document = await ctx.bot.db_client.user_collection.find_one({'user_id': user_id})
+        print(document)
         if not document:
+            print("User not found.")
             pass
+
         command_cooldown = document['cooldowns'].get(ctx.command.name)
+        print()
         if command_cooldown and (last_used := command_cooldown + datetime.timedelta(seconds=per)) > now:
-            print(last_used)
-            print(command_cooldown)
-            target_time = command_cooldown + datetime.timedelta(seconds=per)
-            print(target_time)
-            print(per)
-            local_tz = pytz.timezone('US/Eastern')  # Replace with the actual time zone
-            target_time_local = target_time.astimezone(local_tz)
-            epoch = round(target_time_local.timestamp())
-            print(epoch)
-            embed = discord.Embed(
-                title=f'You are on cooldown. Try again in <t:{epoch}:R>.',
-                color=0x00ff00)
-            await ctx.send(embed=embed)
+            delta = last_used - now
+            await ctx.send(f'You are on cooldown. Try again in {delta.seconds} seconds.')
             return False
 
         await ctx.bot.db_client.user_collection.update_one(
