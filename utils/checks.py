39ece2cfebd4106557625d4e4f89da1723386ef7
Tom <hydrabott@gmail.com>
2023-08-25 11:06:10 -0400

    still wtf but maybe

--- utils/checks.py
+++ utils/checks.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 
 def persistent_cooldown(rate, per, type=commands.BucketType.user):
     async def predicate(ctx, *args, **kwargs):
-        now = discord.utils.utcnow()
+        now = datetime.datetime.now(datetime.timezone.utc)
         user_id = ctx.author.id
 
         document = await ctx.bot.db_client.user_collection.find_one({'user_id': user_id})
