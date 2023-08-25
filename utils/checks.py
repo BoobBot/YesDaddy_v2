0cd5bcbdedbe137c92a496f97b582f638194f868
Tom <hydrabott@gmail.com>
2023-08-25 10:17:17 -0400

    ?

--- utils/checks.py
+++ utils/checks.py
@@ -16,7 +16,7 @@ def persistent_cooldown(rate, per, type=commands.BucketType.user):
         command_cooldown = document['cooldowns'].get(ctx.command.name)
         if command_cooldown and (last_used := command_cooldown + datetime.timedelta(seconds=per)) > now:
             delta = last_used - now
-            target_time = now + delta.seconds
+            target_time = now + delta
             remaining_timestamp = discord.utils.format_dt(target_time, style="R"
             )
             await ctx.send(f'You are on cooldown. Try again in {remaining_timestamp} seconds.')
