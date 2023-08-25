032f38545b42e49b43e2dc17e70d014c2d2dfddb
Tom <hydrabott@gmail.com>
2023-08-25 09:09:10 -0400

    really?

--- utils/checks.py
+++ utils/checks.py
@@ -6,17 +6,14 @@ from discord.ext import commands
 
 def persistent_cooldown(rate, per, type=commands.BucketType.user):
     async def predicate(ctx, *args, **kwargs):
-        now = datetime.datetime.utcnow()
+        now = discord.utils.utcnow()
         user_id = ctx.author.id
 
         document = await ctx.bot.db_client.user_collection.find_one({'user_id': user_id})
-        print(document)
         if not document:
-            print("User not found.")
             pass
 
         command_cooldown = document['cooldowns'].get(ctx.command.name)
-        print()
         if command_cooldown and (last_used := command_cooldown + datetime.timedelta(seconds=per)) > now:
             delta = last_used - now
             target_time = now + delta.seconds
