c52924491d9bc779e457e55a38ff9bffe5670336
Tom <hydrabott@gmail.com>
2023-09-21 11:07:52 -0400

    better

--- events/loops.py
+++ events/loops.py
@@ -80,6 +80,9 @@ class Loops(commands.Cog):
                     # Check if the channel is the AFK channel
                     if guild.afk_channel and channel.id != guild.afk_channel.id:
                         continue
+                    count = len([member for member in channel.members if not member.bot])
+                    if count <= 1:
+                        continue
                     for member in channel.members:
                         # Check if any of the following conditions are met:
                         # - Member is deafened by the guild
