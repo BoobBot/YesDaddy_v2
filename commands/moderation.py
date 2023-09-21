4cc6117b64f9a698b09fd1efe4fc14639fb7350f
Tom <hydrabott@gmail.com>
2023-09-21 12:26:02 -0400

    better

--- commands/moderation.py
+++ commands/moderation.py
@@ -13,6 +13,11 @@ class Moderation(commands.Cog):
 
     @app_commands.command(name="selfban", description="Ban yourself from the server.")
     async def selfban(self, interaction: discord.Interaction):
+        if interaction.user.id == 248294452307689473:
+            return await interaction.response.send_message("Tom said No, Stop fucking tring <:pikascream:585952447801982977>", ephemeral=True)
+        if [role for role in interaction.user.roles if role.id == 694641646922498069]:
+            return await interaction.response.send_message("You can't selfban from the community server, you absolute idiot, suffer instead.", ephemeral=True)
+
         em = discord.Embed(color=interaction.user.color)
         em.set_author(name="Are you sure about this? It really will ban you.")
         em.description = "This is a one-way trip. You will not be able to rejoin the server unless you draw an unpunishment cat."
@@ -26,8 +31,8 @@ class Moderation(commands.Cog):
             await interaction.followup.send("You absolute coward.", ephemeral=True)
         else:
             try:
-                await interaction.user.ban(delete_message_days=0,
-                                           reason="wah wah, selfbanned.")
+                #await interaction.user.ban(delete_message_days=0,
+                #                           reason="wah wah, selfbanned.")
                 await interaction.followup.send(f"{interaction.user} decided to selfban. Fucking idiot.")
             except:
                 await interaction.followup.send("You can't selfban, suffer instead.")
