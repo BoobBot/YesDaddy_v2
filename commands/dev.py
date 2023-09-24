7873d6dce91fe5e8fbc6cd37cb2f30184a599671
Devoxin <lukel9914@gmail.com>
2023-09-24 23:29:10 +0100

    a little more equal?

--- commands/dev.py
+++ commands/dev.py
@@ -87,20 +87,20 @@ class Dev(commands.Cog):
             .convert('RGBA') \
             .resize((target_size, target_size), resample=Image.LANCZOS)  # ensure we load this with an alpha channel
 
-        base = Image.new("RGBA", (300, 160))
+        base = Image.new("RGBA", (300, 150))
         filtered = user_avatar.copy().filter(ImageFilter.GaussianBlur(radius=10))
         base.paste(filtered, (-int((user_avatar.width / 2) - (base.width / 2)), -int((user_avatar.height / 2) - (base.height / 2))), user_avatar)
 
-        self.arc_bar(img=base, xy=(20, 20), size=(140, 140), progress_pc=100,
+        self.arc_bar(img=base, xy=(10, 10), size=(140, 140), progress_pc=100,
                      width=10, fill=(255, 255, 255))
 
-        self.arc_bar(img=base, xy=(20, 20), size=(140, 140), progress_pc=(user_xp / max_xp) * 100,
+        self.arc_bar(img=base, xy=(10, 10), size=(140, 140), progress_pc=(user_xp / max_xp) * 100,
                      width=10, fill=(0, 191, 255))
 
         avatar_circle = user_avatar.copy()
         self.mask_ellipsis(avatar_circle)  # Apply mask before resizing as this yields better quality edges after applying mask
-        avatar_circle = avatar_circle.resize((100, 100), resample=Image.LANCZOS)
-        base.paste(avatar_circle, (30, 30), avatar_circle)
+        avatar_circle = avatar_circle.resize((90, 90), resample=Image.LANCZOS)
+        base.paste(avatar_circle, (20, 20), avatar_circle)
 
         # Add text for XP and Balance
         font = ImageFont.load_default()  # You can choose a different font
