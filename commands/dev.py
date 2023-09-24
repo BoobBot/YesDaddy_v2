fabbdeca059ccb98c080394cf00ec925ea701385
Devoxin <lukel9914@gmail.com>
2023-09-24 23:32:43 +0100

    Adjust avatar position too?

--- commands/dev.py
+++ commands/dev.py
@@ -100,7 +100,7 @@ class Dev(commands.Cog):
         avatar_circle = user_avatar.copy()
         self.mask_ellipsis(avatar_circle)  # Apply mask before resizing as this yields better quality edges after applying mask
         avatar_circle = avatar_circle.resize((110, 110), resample=Image.LANCZOS)
-        base.paste(avatar_circle, (20, 20), avatar_circle)
+        base.paste(avatar_circle, (15, 15), avatar_circle)
 
         # Add text for XP and Balance
         font = ImageFont.load_default()  # You can choose a different font
