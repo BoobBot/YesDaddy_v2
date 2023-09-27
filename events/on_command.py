f9607493f5e9678c74b2f243fe81be67133c714a
Tom <hydrabott@gmail.com>
2023-09-27 07:08:43 -0400

    UTF-8 Maybe

--- events/on_command.py
+++ events/on_command.py
@@ -19,8 +19,7 @@ class OnCommand(commands.Cog):
 
         else:
             destination = '#{0.channel.name} ({0.guild.name})'.format(message)
-        self.bot.log.info('{0.created_at}: {0.author.name} in {1}: {0.content}'.format(message, destination))
-        print('{0.created_at}: {0.author.name} in {1}: {0.content}'.format(message, destination))
+        self.bot.log.info('{3} : {0.created_at}: {0.author.name} in {1}: {0.content}'.format(message, destination, ctx.command))
 
 
 async def setup(bot):
