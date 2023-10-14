d9525bd74a3de7b124bee18fa0dd03e63c13c0e2
Tails <tom@boob.bot>
2023-10-14 17:31:19 -0400

    less fucked

--- views/rule_button_view.py
+++ views/rule_button_view.py
@@ -8,10 +8,11 @@ class RuleButton(discord.ui.View):
     @discord.ui.button(label='I accept the rules', style=discord.ButtonStyle.green,
                        custom_id='memberrole', emoji='✔️')
     async def ruleaccept(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.defer(thinking=True, ephemeral=True)
         # for testing await interaction.client.db_client.delete_guild(interaction.guild.id)
         # Check if the user already has the member role
         if any(r.id == 694641646780022826 for r in interaction.user.roles):
             return await interaction.response.send_message("You already have the member role!", ephemeral=True)
 
         await interaction.user.add_roles(interaction.guild.get_role(694641646780022826))  # Member
-        await interaction.response.send_message("You have received the member role!", ephemeral=True)
+        await interaction.followup.send("You have received the member role!", ephemeral=True)
