12b2ebe66a0ee7f8d90d4c6212264117eed1890a
Tom <hydrabott@gmail.com>
2023-10-17 18:15:01 -0400

    waifu

--- events/ready.py
+++ events/ready.py
@@ -10,16 +10,7 @@ class Ready(commands.Cog):
         self.bot.log.info('Ready!')
         self.bot.log.info(f'Logged in as ----> {self.bot.user}')
         self.bot.log.info(f'ID: {self.bot.user.id}')
-        guild = self.bot.get_guild(694641646780022818)
-        guild_data = await self.bot.db_client.get_guild(guild.id)
-        for w in guild_data.waifus:
-            res = isinstance(w.get("user_id"), str)
-            if res:
-                print(w)
-                #remove
-                guild_data.waifus.remove(w)
-                await self.bot.db_client.update_guild(guild.id, {"waifus": guild_data.waifus})
-
+        self.bot.log.info('------')
 
 async def setup(bot):
     await bot.add_cog(Ready(bot))
