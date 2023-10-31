30e96a593173041ec1fb7a0fe56f15099ef0fdaf
Tom <hydrabott@gmail.com>
2023-10-31 13:21:30 -0400

    lets see what happend here

--- events/ready.py
+++ events/ready.py
@@ -13,12 +13,9 @@ class Ready(commands.Cog):
         self.bot.log.info('------')
         guild = await self.bot.db_client.get_guild(694641646780022818)
         for user in reversed(guild.users):
-            if 'guild_id' not in user:
+            if 'user_id' not in user:
                 print(user)
-                guild.users.remove(user)
-            #if 'user_id' not in user:
-                #print(user)
-                #guild.users.pop(user)
+                guild.users.pop(user)
         await self.bot.db_client.update_guild(guild.guild_id, {'users': guild.users})
         print('Done')
 async def setup(bot):
