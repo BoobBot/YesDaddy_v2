07f68273189a2d49425422b352e4b3f9a1d70b21
Tom <hydrabott@gmail.com>
2023-11-02 11:21:57 -0400

    test yes

--- views/reminder_view.py
+++ views/reminder_view.py
@@ -33,7 +33,7 @@ class Reminder(discord.ui.View):
     async def disable_buttons(self):
         for item in self.children:
             item.style = discord.ButtonStyle.green if self.set else discord.ButtonStyle.red
-            item.emoji = "👍" if self.set else "❌"
+            item.emoji = "👍" if self.set else "✖️"
             item.disabled = True
 
         await self.message.edit(view=self)
