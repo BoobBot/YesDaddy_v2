415d0b09ac60f8f89e8c4fa0c9c6eb4d1d8416dc
Tom <hydrabott@gmail.com>
2023-12-19 10:43:50 -0500

    Introduce Challenge class for daily challenges
    A Challenge class has been introduced in challenge_view.py to replace the previous TextInput component. The class offers a more interactive interface for daily challenges, and includes options for user answers submission and error handling. Users can now directly input their responses through this interface, and receive immediate feedback for their responses.

--- views/challenge_view.py
+++ views/challenge_view.py
@@ -2,19 +2,44 @@ import discord
 from discord.ui import View, Button, TextInput
 
 
+class Challenge(discord.ui.Modal, title='daily challenge'):
+    def __init__(self, ctx, challenge, answer):
+        super().__init__()
+        self.ctx = ctx
+        self.challenge = challenge
+        self.answer = answer
+
+    guess = discord.ui.TextInput(
+        label=self.challenge,
+        style=discord.TextStyle.long,
+        placeholder="Enter your answer",
+        required=True,
+        max_length=300,
+    )
+
+    async def on_submit(self, interaction: discord.Interaction):
+        user_answer = self.guess.value
+        if user_answer.lower() == self.answer.lower():
+            await interaction.response.send_message("Your answer is correct! You win!")
+        else:
+            await interaction.response.send_message("Incorrect answer.")
+
+    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
+        await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
+        self.ctx.bot.log.exception('Error with feedback command.', exc_info=error)
+
+
 class ChallengeView(View):
     def __init__(self, ctx, challenge, answer):
         super().__init__()
         self.ctx = ctx
         self.challenge = challenge
         self.answer = answer
-        self.add_item(Button(style=discord.ButtonStyle.primary, label="Close", custom_id="close_button"))
 
     @discord.ui.button(label='Answer', style=discord.ButtonStyle.success)
     async def answer_button(self, button: discord.ui.Button, interaction: discord.Interaction):
-        input_field = TextInput(placeholder="Enter your answer", custom_id="answer_input")
-        self.add_item(input_field)
-        await interaction.response.edit_message(view=self)
+        await interaction.response.send_modal(Challenge(self.ctx, self.challenge, self.answer)
+                                              )
 
     @discord.ui.button(label='Close', style=discord.ButtonStyle.danger)
     async def close_button(self, button: discord.ui.Button, interaction: discord.Interaction):
@@ -25,17 +50,3 @@ class ChallengeView(View):
         if interaction.user == self.ctx.author:
             return True
         return False
-
-    async def on_error(self, error, item, interaction):
-        if isinstance(error, discord.errors.CheckFailure):
-            await interaction.response.send_message("You cannot interact with this component.")
-
-    @discord.ui.button(label='Submit', style=discord.ButtonStyle.primary, row=1)
-    async def submit_button(self, button: discord.ui.Button, interaction: discord.Interaction):
-        user_answer = interaction.data['values']['answer_input']
-
-        if user_answer.lower() == self.answer.lower():
-            await interaction.response.send_message("Your answer is correct! You win!")
-        else:
-            await interaction.response.send_message("Incorrect answer.")
-
