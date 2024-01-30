3af9913e20d2990183986c1887fe483e43e41f96
Tom <hydrabott@gmail.com>
2024-01-30 15:29:30 -0500

    Enable automatic deletion of inactive users
    The commented out code that would automatically delete guild users after 60 days of inactivity has been uncommented. This change re-enables automatic deletion for inactive users, ensuring that users who have not been active in the last 60 days will be removed from the guild.

--- events/ready.py
+++ events/ready.py
@@ -14,31 +14,31 @@ class Ready(commands.Cog):
         self.bot.log.info(f'Logged in as ----> {self.bot.user}')
         self.bot.log.info(f'ID: {self.bot.user.id}')
         self.bot.log.info('------')
-        # guild = await self.bot.db_client.get_guild(694641646780022818)
-        # ic = 0
-        # for user in guild.users:
-        #     last_seen = user.get('last_seen', None)
-        #     if last_seen:
-        #         if isinstance(last_seen, datetime.datetime):  # if last_seen is already a datetime object
-        #             date = last_seen.replace(tzinfo=datetime.timezone.utc)
-        #         else:  # assuming last_seen is an integer
-        #             try:
-        #                 date = datetime.datetime.fromtimestamp(last_seen).replace(tzinfo=datetime.timezone.utc)
-        #             except TypeError:
-        #                 self.bot.log.error(
-        #                     f"Invalid 'last_seen' value for user: {user}")  # replace with actual user identification
-        #                 continue  # continue with next user
-        #
-        #         current_date = datetime.datetime.now().replace(tzinfo=datetime.timezone.utc)
-        #         days_difference = (current_date - date).days
-        #         if days_difference >= 60:
-        #             print(f"deleting {user.get('user_id')}")
-        #             await guild.delete_user(user.get('user_id'))
-        #             print("deleted")
-        #             ic += 1
-        #     else:
-        #         print("no last seen")
-        # print(ic)
+        guild = await self.bot.db_client.get_guild(694641646780022818)
+        ic = 0
+        for user in guild.users:
+            last_seen = user.get('last_seen', None)
+            if last_seen:
+                if isinstance(last_seen, datetime.datetime):  # if last_seen is already a datetime object
+                    date = last_seen.replace(tzinfo=datetime.timezone.utc)
+                else:  # assuming last_seen is an integer
+                    try:
+                        date = datetime.datetime.fromtimestamp(last_seen).replace(tzinfo=datetime.timezone.utc)
+                    except TypeError:
+                        self.bot.log.error(
+                            f"Invalid 'last_seen' value for user: {user}")  # replace with actual user identification
+                        continue  # continue with next user
+
+                current_date = datetime.datetime.now().replace(tzinfo=datetime.timezone.utc)
+                days_difference = (current_date - date).days
+                if days_difference >= 60:
+                    print(f"deleting {user.get('user_id')}")
+                    await guild.delete_user(user.get('user_id'))
+                    print("deleted")
+                    ic += 1
+            else:
+                print("no last seen")
+        print(ic)
 
 async def setup(bot):
     await bot.add_cog(Ready(bot))
