238dfc8aeeb462120105eb6707f73c66e8bc1995
Tails <tom@boob.bot>
2024-11-10 16:35:13 -0500

    Add reaction for bad_flag in message content
    This commit introduces a check for the presence of bad_flag in the message content and adds a reaction with good_flag if found. This enhances user interactions by providing immediate feedback on flagged content. Additionally, it imports the good_flag utility to support this functionality.

--- events/message.py
+++ events/message.py
@@ -6,7 +6,8 @@ import random
 import discord
 from discord.ext import commands
 
-from utils.utilities import calculate_level, is_today_weekend_or_holiday, amount_on_level_up, bad_flag, swap_flag
+from utils.utilities import calculate_level, is_today_weekend_or_holiday, amount_on_level_up, bad_flag, swap_flag, \
+    good_flag
 
 
 async def dump_delete(msg):
@@ -53,6 +54,8 @@ class Message(commands.Cog):
                         await channel.send(attachment.url)
                 return
         if msg.guild:
+            if bad_flag in msg.content:
+                await msg.add_reaction(good_flag)
             if bad_flag in msg.author.display_name:
                 new_name = swap_flag(msg.author.display_name)
                 await msg.author.edit(nick=new_name)
