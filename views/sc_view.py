8fbd28958a3f41838c3794c191c33d6955ef16be
Tom <hydrabott@gmail.com>
2025-06-04 10:57:20 -0400

    Set timeout to None in ReferralView initialization
    The timeout parameter is explicitly set to None in the ReferralView constructor. This ensures the view remains active indefinitely unless manually stopped.

--- views/sc_view.py
+++ views/sc_view.py
@@ -6,7 +6,7 @@ import discord
 
 class ReferralView(discord.ui.View):
     def __init__(self):
-        super().__init__()
+        super().__init__(timeout=None)
         self.add_item(discord.ui.Button(
             label="Join Star Citizen 🌌",
             url="https://www.robertsspaceindustries.com/enlist?referral=STAR-9YPL-WBP9"
